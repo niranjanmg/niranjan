e10216577724b340980c8103be627f8f5de46cc2
SHANTHOSH <SHANTHOSH@SHANTHOSH-PC>
2020-01-10 15:00:02 +0530

    this is my first commit

---
+++ FirstgitHub/src/org/studyeasygithub/FirstGitHub.java
@@ -0,0 +1,10 @@
+package org.studyeasygithub;
+
+public class FirstGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my firstgithub export to github");
+	}
+
+}
